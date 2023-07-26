f299d1fdd7f8d1ba7e04337728a0ea0deda30b75
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-26 16:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function calculateMinCost() {
   //your code here
-  
-  
+	const inputEle = document.getElementById('rope-lengths');
+	const resultEle = document.getElementById('result');
+	
+	const ropeLengths = inputEle.value.split(',')
+        .map(length => parseInt(length.trim()));
+
+	let result = [];
+	let n = ropeLengths.length;
+	
+	
+	// n
+	while(n > 1){
+	  
+	  ropeLengths.sort((a,b) => a-b)
+	  const sum = ropeLengths[1] + ropeLengths[0]
+	  ropeLengths[1] = sum
+	  ropeLengths.shift()
+	  result.push(sum)
+	  n--;
+	}
+	
+	let ans = 0; 
+	result.forEach((element) => {
+	  ans+=element
+	})
+
+	resultEle.innerText = `Minimum cost of connecting ropes: ${ans}`;
   
 }  
